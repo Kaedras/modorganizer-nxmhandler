c827238e2077c2327ef6b0a300e21b6c76c70a0a
Kaedras <6783810+Kaedras@users.noreply.github.com>
2026-02-20 12:32:28 +0100

    Change `handlerRegKey` type to `QString`

--- src/utility.h
+++ src/utility.h
@@ -3,10 +3,10 @@
 #include <QString>
 
 #ifdef _WIN32
-static constexpr const char* handlerRegKey = "shell/open/command/Default";
+static const auto handlerRegKey = QStringLiteral("shell/open/command/Default");
 static constexpr const char* fileDialogFilter = "Executable (*.exe)";
 #else
-static constexpr const char* handlerRegKey = "Desktop Entry/Exec";
+static const auto handlerRegKey = QStringLiteral("Desktop Entry/Exec");
 static constexpr const char* fileDialogFilter = "Executable";
 #endif
 
